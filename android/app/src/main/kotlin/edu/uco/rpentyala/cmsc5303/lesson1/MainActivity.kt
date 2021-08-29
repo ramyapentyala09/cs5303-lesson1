649c4074641af0ff62315e7daa01e77b780eca55
Ramya Pentyala <ramyapentyala@uco.edu>
2021-08-29 00:16:28 -0500

    initial commit

---
+++ android/app/src/main/kotlin/edu/uco/rpentyala/cmsc5303/lesson1/MainActivity.kt
@@ -0,0 +1,6 @@
+package edu.uco.rpentyala.cmsc5303.lesson1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(new Lesson1App());
+}
+
+class Lesson1App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text('Hello World! You!'),
+    );
+  }
+  
+}
